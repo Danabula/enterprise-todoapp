2f2443c3f599a1ef31a6210984460ec35780ad1a
Daan P <d.pols@rcnheliomare.nl>
2024-04-08 08:02:28 +0200

    api input validation

--- src/routes/api/create/+server.ts
+++ src/routes/api/create/+server.ts
@@ -1,11 +1,29 @@
 import prisma from '$lib/prisma';
 import type { RequestHandler } from './$types';
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const data = await request.json();
+
+	if (!data.title) {
+		return error(400, 'title is required.');
+	} else if (typeof data.title !== 'string') {
+		return error(400, 'title must be of type string.');
+	}
+
+	if (data.description) {
+		if (typeof data.description !== 'string') {
+			return error(400, 'description must be of type string.');
+		}
+	} else {
+		data.description = null;
+	}
+
 	const result = await prisma.todo.create({
-		data
+		data: {
+			title: data.title,
+			description: data.description
+		}
 	});
 	return json(result);
 };
